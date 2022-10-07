fd76452e7cd1085182b306ec13a2a39fd156dbf7
hcx_xlj <2088361912@qq.com>
2022-10-07 15:38:02 +0800

    token 过期时间改为7天

--- property-admin/src/main/java/com/ruoyi/web/controller/weixin/WxLoginController.java
+++ property-admin/src/main/java/com/ruoyi/web/controller/weixin/WxLoginController.java
@@ -1,14 +1,10 @@
 package com.ruoyi.web.controller.weixin;
 
-import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.domain.AjaxResult;
-import com.ruoyi.common.enums.BusinessType;
 import com.ruoyi.framework.web.service.SysLoginService;
-import com.ruoyi.system.service.ISysUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
