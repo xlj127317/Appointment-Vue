860944d30d7466ddf988a1887cf703c80c9c69aa
k000 <linmohc@gmail.com>
2022-10-08 20:54:33 +0800

    应收管理补充字段

--- property-web/src/main/java/com/ruoyi/property/dto/ReceivableOutput.java
+++ property-web/src/main/java/com/ruoyi/property/dto/ReceivableOutput.java
@@ -21,4 +21,22 @@ public class ReceivableOutput {
     private Integer advanceStatus;
     private String deductMoney;
     private String deductAfterMoney;
+
+    private Date deductDate;
+
+    private Integer actuallyStatus;
+
+    private String actuallyMoney;
+    private Date actuallyDate;
+    private Date stopTime;
+    private Integer arrearsStatus;
+    private String arrearsMoney;
+
+    private Date repairDate;
+
+    private Integer paymentType;
+    private Integer refundStatus;
+
+    private String creatorName;
+    private Date createTime;
 }
