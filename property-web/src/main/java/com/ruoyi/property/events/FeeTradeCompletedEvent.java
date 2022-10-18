82bfc8387a82afa27b5e383b0466543ab3ed3b14
k000 <linmohc@gmail.com>
2022-10-19 07:02:41 +0800

    fixup

--- property-web/src/main/java/com/ruoyi/property/events/FeeTradeCompletedEvent.java
+++ property-web/src/main/java/com/ruoyi/property/events/FeeTradeCompletedEvent.java
@@ -2,14 +2,8 @@ package com.ruoyi.property.events;
 
 import com.ruoyi.property.dto.FeeTradeOutputDto;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.context.ApplicationEvent;
 
 @Data
-public class FeeTradeCompletedEvent extends ApplicationEvent {
+public class FeeTradeCompletedEvent {
     private FeeTradeOutputDto feeTrade;
-
-    public FeeTradeCompletedEvent(Object source) {
-        super(source);
-    }
 }
